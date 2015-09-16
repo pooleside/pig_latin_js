2d1c25dbd0627f281e2b8885b6d21b0115e62ba5
pooleside <lpoole26@gmail.com>
2015-09-16 11:55:57 -0700

    "next spec is passing"

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 var startsWithVowel= function(userWord) {
-  return userWord + "ay";
+  if (userWord[0] === 'a' || userWord[0] === 'e' || userWord[0] === 'i' ||userWord[0] === 'o' ||userWord[0] === 'u') {
+    return userWord + "ay";
+  } else {
+    return userWord.slice(1) + userWord[0] + 'ay';
+  }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -2,10 +2,8 @@ describe('startsWithVowel', function() {
   it("adds ay to the end of a word that starts with a vowel", function() {
     expect(startsWithVowel("eat")).to.equal("eatay");
   });
-});
 
-describe('moveConsonant', function() {
   it("takes the consonant at the beginning of a word and moves it to the end", function() {
-    expect(moveConsonant("car")).to.equal("arc");
+    expect(startsWithVowel("car")).to.equal("arcay");
   });
 });
