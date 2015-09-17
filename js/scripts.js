b006ec651fe9986edd24135847c786dc84562283
pooleside <lpoole26@gmail.com>
2015-09-16 17:28:59 -0700

    "refactoring code"

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
 var translator = function(userWord) {
-  var vowels = ['a', 'e', 'i', 'o', 'u'];
-  for (var i = 0; i < vowels.length; i++) {
-    if(userWord[0] === vowels[i]) {
-      var startsWithVowel = true;
-        break;
-      }
-  }
-
-    if (startsWithVowel) {
-    var pigLatinWord = userWord;
-  } else {
-    var pigLatinWord = userWord.slice(1) + userWord[0];
-  }
-    return pigLatinWord + "ay";
+  var splitAt = indexOfFirstVowel(userWord); // split at the index of the first vowel
+  return userWord.slice(splitAt) + userWord.slice(0, splitAt) + 'ay';
 };
+
+  var indexOfFirstVowel = function(word) {  //find index of first vowel by having a list of vowels
+    var vowels = ['a', 'e', 'i', 'o', 'u'];
+    var letters = word.split("");         //split the word into letters
+
+    for (var i = 0; i < letters.length; i++) {  //loop through all the letters
+      for (var j = 0; j < vowels.length; j++) {  //loop through all the vowels
+          if (letters[i] === vowels[j]) {  //when we find they are equal
+            return i                        //we return the index of the letter we are at
+          }
+        }                         //then...back to top.  We build pigLatin by sliceing it up
+      }
+  };
--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,18 @@ describe('translator', function() {
   it("takes the consonant at the beginning of a word and moves it to the end", function() {
     expect(translator("car")).to.equal("arcay");
   });
+
+  it("takes two consonants at the beginning of a word and moves to the end", function() {
+    expect(translator("thunder")).to.equal("underthay");
+  });
+});
+
+describe("indexOfFirstVowel", function() {
+  it("returns 0 for a word that starts with a vowel", function() {
+    expect(indexOfFirstVowel("apple").to.equal(0));
+  });
+
+  it("returns 1 for a word that starts with a single consonant", function() {
+    expect(indexOfFirstVowel("cat").to.equal(1));
+  });
 });
